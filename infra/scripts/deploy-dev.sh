fc7c1a54e2cd55ac9d8774fa5cdbf6b95f8dcdac
GonzaOrtellado <200260358+xshrp@users.noreply.github.com>
2026-06-23 18:06:04 -0300

    fix: path fix (again)

--- infra/scripts/deploy-dev.sh
+++ infra/scripts/deploy-dev.sh
@@ -9,47 +9,47 @@ echo "Iniciando deploy en el cluster 'develop'"
 echo "----------------------------------------"
 
 echo "Namespace (1/11)"
-kubectl apply -f "$BASE_DIR/k8s/base/namespace.yaml"
+kubectl apply -f "$BASE_DIR/infra/k8s/base/namespace.yaml"
 
 echo ""
 echo "Redis (2/11)"
-kubectl apply -f "$BASE_DIR/k8s/base/redis.yaml"
+kubectl apply -f "$BASE_DIR/infra/k8s/base/redis.yaml"
 
 echo ""
 echo "Postgres (3/11)"
-kubectl apply -f "$BASE_DIR/k8s/base/postgres.yaml"
+kubectl apply -f "$BASE_DIR/infra/k8s/base/postgres.yaml"
 
 echo ""
 echo "Servicios (4/11)"
-kubectl apply -f "$BASE_DIR/k8s/base/services.yaml"
+kubectl apply -f "$BASE_DIR/infra/k8s/base/services.yaml"
 
 echo ""
 echo "Catalog (5/11)"
-kubectl apply -f "$BASE_DIR/k8s/base/catalog.yaml"
+kubectl apply -f "$BASE_DIR/infra/k8s/base/catalog.yaml"
 
 echo ""
 echo "Cart (6/11)"
-kubectl apply -f "$BASE_DIR/k8s/base/cart.yaml"
+kubectl apply -f "$BASE_DIR/infra/k8s/base/cart.yaml"
 
 echo ""
 echo "Orders (7/11)"
-kubectl apply -f "$BASE_DIR/k8s/base/orders.yaml"
+kubectl apply -f "$BASE_DIR/infra/k8s/base/orders.yaml"
 
 echo ""
 echo "Checkout (8/11)"
-kubectl apply -f "$BASE_DIR/k8s/base/checkout.yaml"
+kubectl apply -f "$BASE_DIR/infra/k8s/base/checkout.yaml"
 
 echo ""
 echo "UI (9/11)"
-kubectl apply -f "$BASE_DIR/k8s/base/ui.yaml"
+kubectl apply -f "$BASE_DIR/infra/k8s/base/ui.yaml"
 
 echo ""
 echo "Admin (10/11)"
-kubectl apply -f "$BASE_DIR/k8s/base/admin.yaml"
+kubectl apply -f "$BASE_DIR/infra/k8s/base/admin.yaml"
 
 echo ""
 echo "Ingress (11/11)"
-kubectl apply -f "$BASE_DIR/k8s/base/ingress.yaml"
+kubectl apply -f "$BASE_DIR/infra/k8s/base/ingress.yaml"
 
 echo "----------------------------------------"
 echo "Deploy de 'develop' finalizado"
