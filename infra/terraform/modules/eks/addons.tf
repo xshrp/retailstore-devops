44c1bcde445e08be78315afdd40e175b85c2b253
GonzaOrtellado <200260358+xshrp@users.noreply.github.com>
2026-06-23 20:30:49 -0300

    feat: aws_eks_addon deleted

--- infra/terraform/modules/eks/addons.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_eks_addon" "ebs_csi" {
-  cluster_name                = aws_eks_cluster.this.name
-  addon_name                  = "aws-ebs-csi-driver"
-  resolve_conflicts_on_create = "OVERWRITE"
-  resolve_conflicts_on_update = "OVERWRITE"
-}
