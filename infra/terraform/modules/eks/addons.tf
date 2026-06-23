146959a0cd950bb670d7817ce958ce7fd6c0ca9c
GonzaOrtellado <200260358+xshrp@users.noreply.github.com>
2026-06-23 20:13:48 -0300

    feat: aws_eks_addon add for ebs_csi volumes

---
+++ infra/terraform/modules/eks/addons.tf
@@ -0,0 +1,6 @@
+resource "aws_eks_addon" "ebs_csi" {
+  cluster_name                = aws_eks_cluster.this.name
+  addon_name                  = "aws-ebs-csi-driver"
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+}
