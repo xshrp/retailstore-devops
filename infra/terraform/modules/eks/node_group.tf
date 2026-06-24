b1a051df11c9175bbf082d8596ca3204c6c15575
GonzaOrtellado <200260358+xshrp@users.noreply.github.com>
2026-06-23 21:32:21 -0300

    fix: deleted volume

--- infra/terraform/modules/eks/node_group.tf
+++ infra/terraform/modules/eks/node_group.tf
@@ -7,9 +7,9 @@ resource "aws_eks_node_group" "this" {
   subnet_ids = var.private_subnets
 
   scaling_config {
-    desired_size = 3
-    min_size     = 2
-    max_size     = 5
+    desired_size = 1
+    min_size     = 1
+    max_size     = 2
   }
 
   instance_types = [var.node_instance_type]
