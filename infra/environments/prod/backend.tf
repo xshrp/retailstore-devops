4f64b29fbe1b95add7e544b15f4d80f30746e6f0
Gonza <200260358+xshrp@users.noreply.github.com>
2026-06-29 13:15:49 -0300

    Change S3 bucket for production environment

--- infra/environments/prod/backend.tf
+++ infra/environments/prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "devops-retail-gj-bucket"
+    bucket = "devops-retail-gj-bucket-prod"
     key    = "prod/terraform.tfstate"
     region = "us-east-1"
   }
