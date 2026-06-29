c0a6be850a0e74a751f7eb9775cb616f15889959
Gonza <200260358+xshrp@users.noreply.github.com>
2026-06-29 13:25:09 -0300

    fix: bucket name

--- infra/environments/prod/backend.tf
+++ infra/environments/prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "devops-retail-gj-bucket-TODO"
+    bucket = "devops-retail-gj-bucket"
     key    = "prod/terraform.tfstate"
     region = "us-east-1"
   }
